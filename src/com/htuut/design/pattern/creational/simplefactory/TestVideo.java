4b6ea3b69ae80a1d410be61f8afb3efddf9cac6e
fangwu <fangwu@lacesar.com>
2019-04-21 10:17:03 +0800

    反射的方式创建对象

--- src/com/htuut/design/pattern/creational/simplefactory/TestVideo.java
+++ src/com/htuut/design/pattern/creational/simplefactory/TestVideo.java
@@ -4,11 +4,13 @@ public class TestVideo {
 
     public static void main(String[] args) {
 
+        //1、使用类型的方式， 优点是：不用导入自类的包，减少依赖
         Video java = VideoFactory.getInstance("java");
-
         java.produce();
 
+        //2、使用反射的方式， 优点是：扩展性强
+        Video python = VideoFactory.getInstance(PathonVideo.class);
+        python.produce();
+
     }
-
-
 }
--- src/com/htuut/design/pattern/creational/simplefactory/VideoFactory.java
+++ src/com/htuut/design/pattern/creational/simplefactory/VideoFactory.java
@@ -17,4 +17,22 @@ public class VideoFactory {
 
     }
 
+    public static Video getInstance(Class<? extends Video> clazz) {
+
+        Video video = null;
+
+        try {
+            video = (Video) Class.forName(clazz.getName()).newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return video;
+
+    }
+
 }
